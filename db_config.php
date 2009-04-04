0314e4abc47bdc30f75bfda1805145cb963cdca0
thinkcmscn <thinkcmscn@982222a4-d8c6-11dd-93da-dbb4cac96d7c>
2009-04-04 02:52:52 +0000

    ThinkCMS V1.0 Final

---
+++ db_config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	/* 数据库 设定 */
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'localhost',
+    'DB_NAME'=> 'thinkcms',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'',
+    'DB_PORT'=>'',
+    'DB_PREFIX'=>'thinkcms_',
+    'DB_CHARSET'=>'UTF8',
+);
+?>
